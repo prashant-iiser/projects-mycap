5fa98be9d43636b3aa278119b1a0fda10cd33c03
prashant-iiser <67929645+prashant-iiser@users.noreply.github.com>
2020-07-26 21:18:43 +0530

    Add files via upload

---
+++ positivenum in.py
@@ -0,0 +1,12 @@
+list1 = [12,-7,5,64,-14]
+for num in list1:
+    if num > 0:
+        print(num, end=" ")
+
+print(" ")
+
+
+list2 = [12,14,-95,3]
+for n in list2:
+    if n>0:
+        print(n, end=" ")
