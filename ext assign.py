8211b15c7d3a4574109b61dfac6ada239322d66d
prashant-iiser <67929645+prashant-iiser@users.noreply.github.com>
2020-07-26 21:16:31 +0530

    Add files via upload

---
+++ ext assign.py
@@ -0,0 +1,16 @@
+  
+x=input("Enter the extension: ")
+mydic={
+    '.txt': 'text file',
+    '.dat': 'data file',
+    '.py': 'python file',
+    '.doc':'document file',
+    '.zip':'zip file',
+    '.mp3':'MP3 audio file',
+    '.pkg':'package file',
+    '.gif':'GIF file',
+    '.html':'HTML file',
+    '.ppt':'Power point presentation',
+    '.h':'Header file'
+    }
+print(mydic[x])
